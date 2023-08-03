d3165a915e58849426bcc2d8a9d20750ec21a504
Michele Ascoli <micheleascoli17@gmail.com>
2023-08-03 14:46:07 -0500

    Iterando em lista com While

---
+++ main.js
@@ -0,0 +1,17 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+   tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador++;
+}
